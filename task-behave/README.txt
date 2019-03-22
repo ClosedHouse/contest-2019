c6c17ba84dfd75b4d145fb827a39665f9cd66101
ksrot <ksrot@redhat.com>
2019-03-22 14:00:23 +0100

    another rewrite of task-behave

---
+++ task-behave/README.txt
@@ -0,0 +1,11 @@
+Start with running commands
+  # task-behave --intro
+  # task-behave --desc
+
+------------------------------------
+Pro komunikaci v cestine spust prikaz
+  # task-behave --set-lang cz
+
+Zacni spustenim prikazu
+  # task-behave --intro
+  # task-behave --desc
--- task-behave/task-behave.py
+++ task-behave/task-behave.py
@@ -10,7 +10,7 @@ import sys
 import argparse
 
 DBPATH='/var/lib/misc/task-behave.db'
-LANG="cz" # or en
+LANG="en" # or cz
 MSGS = {}
 MSGS['cz'] = {
     'CREATE_TESTDIR': 'Vytvoren novy testovaci adresar',
@@ -23,6 +23,8 @@ MSGS['cz'] = {
     'SOLUTION_CHECK': "Overuji spravnost implementace testu",
     'SOLUTION_OK': "Gratuluji! Tajny kod pro tuto uroven je",
     'SOLUTION_NOK': "Bohuzel, snazte se lepe.",
+    'LANG_NOK': "Nepodporovany jazyk, moznosti jsou",
+    'LANG_SET': "Jazyk byl nastaven na",
     }
 MSGS['en'] = {
     'CREATE_TESTDIR': 'Creating new directory with tests',
@@ -34,7 +36,9 @@ MSGS['en'] = {
     'TEST4': 'Running additional test scenarios',
     'SOLUTION_CHECK': "Validating your test implementation",
     'SOLUTION_OK': "Congratulations! The secred code for this level is",
-    'SOLUTION_NOK': "This is not good enough, keep trying!",
+    'SOLUTION_NOK': "This is not good enough, keep on trying!",
+    'LANG_NOK': "Unsupported language, choose from",
+    'LANG_SET': "Language has been changed to",
     }
 
 
@@ -48,13 +52,14 @@ def encode_text(text):
     return base64.b64encode(text.encode())
 
 def decode_text(text):
+    s = base64.b64decode(text)
     return base64.b64decode(text).decode()
 
 def initDB():
     db = {}
-    for filename in ['file.feature', 'grep.feature', 'correct-grep.feature', 'failing-steps.feature', 'zadani', 'napoveda1', 'napoveda2', 'description', 'hint1', 'hint2', 'level_code', 'generic_steps.py']:
+    for filename in ['file.feature', 'grep.feature', 'correct-grep.feature', 'failing-steps.feature', 'description.en', 'description.cz', 'hint1.en', 'hint2.en', 'hint1.cz', 'hint2.cz', 'level_code', 'generic_steps.py', 'introduction.en', 'introduction.cz', 'README.txt']:
         db[filename] = encode_text(read_file(filename))
-    db['LANG'] = LANG
+    db['LANG'] = encode_text(LANG)
     f = open(DBPATH, 'wb')
     pickle.dump(db, f)
     f.close()
@@ -66,8 +71,15 @@ def readDB():
     f.close()
     for k in db_enc:
         db_dec[k] = decode_text(db_enc[k])
-    LANG=db['LANG']
     return db_dec
+
+def saveDB(db):
+    db_enc = {}
+    for k in db:
+        db_enc[k] = encode_text(db[k])
+    f = open(DBPATH, 'wb')
+    pickle.dump(db_enc, f)
+    f.close()
 
 def write_file(text, path):
     f = open(path, 'w')
@@ -81,7 +93,7 @@ def run_behave_scenario(feature_file, tmpdir):
 
 def init_workdir(workdir):
     os.makedirs(os.path.join(workdir, 'steps'))
-    for name in ['file.feature', 'grep.feature']:
+    for name in ['file.feature', 'grep.feature', 'README.txt']:
         write_file(db[name], os.path.join(workdir, name))
     write_file(db['generic_steps.py'], os.path.join(workdir, 'steps', 'generic_steps.py'))
     print("%s %s" % (MSGS[LANG]['CREATE_TESTDIR'], workdir))
@@ -124,10 +136,85 @@ def test_solution(workdir):
 
 
 # parse arguments
-initDB()
-db = readDB()
+parser = argparse.ArgumentParser(description='Behave exercise.')
+parser.add_argument('--init-db', dest='initdb', action='store_true',
+                    default=False, help=argparse.SUPPRESS)
+parser.add_argument('--set-lang', nargs=1, dest='lang', action='store',
+                    default=[], help="Set language of text outputs (%s)" % ','.join(MSGS.keys()))
+parser.add_argument('--intro', dest='intro', action='store_true',
+                    default=False, help="print introduction")
+parser.add_argument('--desc', dest='desc', action='store_true',
+                    default=False, help="print task description")
+parser.add_argument('--hint1', dest='hint1', action='store_true',
+                    default=False, help="print 1st hint")
+parser.add_argument('--hint2', dest='hint2', action='store_true',
+                    default=False, help="print 2nd hint")
+parser.add_argument('--setup', dest='setup', nargs='?', action='store', metavar='DIR',
+                    default="NA", help="setup the task in a given directory (current by default)")
+parser.add_argument('--eval', dest='eval', nargs='?', action='store', metavar='DIR',
+                    default="NA", help="evaluate the solution in a given directory (current by default)")
+args = parser.parse_args()
 
-workdir = tempfile.mkdtemp()
-init_workdir(workdir)
-test_solution(workdir)
+if args.initdb:
+  initDB()
+
+if not os.path.isfile(DBPATH):
+    print('database missing, did you run --init-db')
+    sys.exit(1)
+
+db = readDB()
+LANG=db['LANG']
+
+if args.lang:
+    if args.lang[0] in MSGS.keys():
+        db['LANG'] = args.lang[0]
+        LANG = args.lang[0]
+        saveDB(db)
+        print("%s %s" % (MSGS[LANG]['LANG_SET'], LANG))
+        sys.exit(0)
+    else:
+        print("%s %s" % (MSGS[LANG]['LANG_NOK'], ', '.join(MSGS.keys())))
+        sys.exit(1)
+
+if args.hint1:
+    key = 'hint1.%s' % LANG
+    if key not in db:
+        key = 'hint1.en'
+    print(db[key])
+    sys.exit(0)
+
+if args.hint2:
+    key = 'hint2.%s' % LANG
+    if key not in db:
+        key = 'hint2.en'
+    print(db[key])
+    sys.exit(0)
+
+if args.intro:
+    key = 'introduction.%s' % LANG
+    if key not in db:
+        key = 'introduction.en'
+    print(db[key])
+    sys.exit(0)
+
+if args.desc:
+    key = 'description.%s' % LANG
+    if key not in db:
+        key = 'description.en'
+    print(db[key])
+    sys.exit(0)
+
+if args.setup != "NA":
+    if args.setup:
+        workdir = args.setup
+    else:
+        workdir = "."
+    init_workdir(workdir)
+
+if args.eval != "NA":
+    if args.eval:
+        workdir = args.eval
+    else:
+        workdir = "."
+    test_solution(workdir)
 
