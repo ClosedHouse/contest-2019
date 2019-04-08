1dacf2170a9a044151e94c101151797a372d3626
Jakub Heger <jheger@redhat.com>
2019-04-08 17:49:56 +0200

    level2: add grade script
    first version of task grading script added(not yet compiled)
    added encoder helper script for grading script
    install script enhanced
    cleanup

--- install_level2.sh
+++ install_level2.sh
@@ -11,18 +11,21 @@ TESTDIR="$HOMEDIR/$LEVELNAME"
 # Cleanup before fresh install
 rm -rf "$TESTDIR"
 sed -i "\%$TESTUSER ALL= NOPASSWD: /usr/bin/dnf%d" /etc/sudoers
+dnf remove -y factorial
 
 # Install
 # adding testuser to sudoers
 echo "$TESTUSER ALL= NOPASSWD: /usr/bin/dnf" >> /etc/sudoers
+# installing "broken" version of factorial package
+dnf install -y "$LEVELNAME/src/factorial-1.0-1.fc29.noarch.rpm"
 
 # creating testdir and copying test files
 mkdir -p "$TESTDIR"
 cd "$LEVELNAME/src"
-cp -r factorial* build_package.sh rpmbuild/ "$TESTDIR"
+cp -r factorial* build_package.sh rpmbuild/ grade "$TESTDIR"
 cp -r tests/factorial "$TESTDIR/test"
 cd ../doc
 # TODO docs
 cd ..
 
-echo "Done"
+echo "$LEVELNAME install finished"
