5a27174d9ccbb6787a26dd3d8ebf2bc7adf3b267
Anna Khaitovich <akhaitov@redhat.com>
2019-04-10 13:09:53 +0200

    level2: edit and fix some tests

--- level2/src/test/runtest.sh
+++ level2/src/test/runtest.sh
@@ -12,72 +12,37 @@
 . /usr/bin/rhts-environment.sh || exit 1
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
-PACKAGE=${PACKAGE:-"math-functions"}
-# TODO je to dobry zpusob predavat data o tom, jake rpm testujeme?
-RPM_PATH=${RPM_PATH:-}
-
 rlJournalStart
     rlPhaseStartSetup
-        # This section installs package under test if it's not already
-        # installed and performs general enviromental setup
-        # Tato sekce instaluje balicek, ktery chceme testovat (pokud jeste
-        # nainstalovany neni) a obecne nastavuje system
+        # This section performs general enviromental setup
+        #
+        # Tato sekce obecne nastavuje system
         rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
         rlRun "pushd $TmpDir"
     rlPhaseEnd
 
-    rlPhaseStartTest "No arguments"
-        # This section checks if running 'factorial' with no arguments works
-        # Tato sekce testuje to, jak funguje zpusteni prikazu 'factorial' s
-        # zadnymi argumenty
-        rlRun "factorial 2>&1 | tee log" 0 "Run factorial with no parameters"
-        rlAssertGrep "Usage: factorial NUMBER" "log"
-    rlPhaseEnd
-
-    rlPhaseStartTest "Help"
-        # This section checks if running 'factorial --help' works
-        # Tato sekce testuje to, jak funguje napoveda ('factorial --help')
-        rlRun "factorial --help 2>&1 | tee log" 0 "Run factorial with no parameters"
-        rlAssertGrep "Usage: factorial NUMBER" "log"
-    rlPhaseEnd
-
-    rlPhaseStartTest "Positive numbers"
-        rlRun "factorial 1 2>&1 | tee log" 0 "Run factorial on number 1"
-        rlAssertGrep "1" "log"
-        rlRun "factorial 2 2>&1 | tee log" 0 "Run factorial on number 2"
-        rlAssertGrep "2" "log"
-        rlRun "factorial 5 2>&1 | tee log" 0 "Run factorial on number 5"
-        rlAssertGrep "120" "log"
-        rlRun "factorial 10 2>&1 | tee log" 0 "Run factorial on number 10"
-        rlAssertGrep "3628800" "log"
-    rlPhaseEnd
-
-    rlPhaseStartTest "Negative numbers"
-        #TODO: Zaporna cisla nefunfujou :(
-        rlRun "factorial -1 2>&1 | tee log" 0 "Run factorial on number -1"
-        rlAssertGrep "-1" "log"
-        rlRun "factorial -2 2>&1 | tee log" 0 "Run factorial on number -2"
-        rlAssertGrep "-2" "log"
-        rlRun "factorial 5 2>&1 | tee log" 0 "Run factorial on number -5"
-        rlAssertGrep "-120" "log"
-        rlRun "factorial 10 2>&1 | tee log" 0 "Run factorial on number -10"
-        rlAssertGrep "-3628800" "log"
-    rlPhaseEnd
-
-    rlPhaseStartTest "Zero"
-        #TODO: faktorial nuly je skutecne 1, ale toto pada na new
-        rlRun "factorial 0 2>&1 | tee log" 0 "Run factorial on number 0"
-        rlAssertGrep "1" "log"
-    rlPhaseEnd
-
-    rlPhaseStartTest "Non-numbers"
-        #TODO: "Exiting unsuccessfully", ale vraci 0?
-        rlRun "factorial foo 2>&1 | tee log" 0 \
+    rlPhaseStartTest "Factorial"
+        # This section tests factorial package.
+        # There are 7 assertions - commands, that execute specific actions and compare
+        # the result of it with the expected one.
+        #
+        # Tato sekce testuje balicek factorial.
+        # Tady se pouziva 7 assertions - prikazu, ktere vykonavaji nejaky cin a
+        # porovnavaji jeho vysledek s vysledkem, ktery ocekavame.
+        rlRun "factorial 2>&1 | grep \"No argument given!\"" 0 "Run factorial with no parameters"
+        rlRun "factorial --help | grep \"Usage: factorial INTEGER\"" 0 "Run factorial --help"
+        rlAssertEquals "Calculate !1" $(factorial 1) 1 
+        rlAssertEquals "Calculate !5" $(factorial 5) 120
+        rlAssertEquals "Calculate !0" $(factorial 0) 1
+        rlRun "factorial -1 2>&1 | grep \"<not_defined>\"" 0 "Run factorial on negative number"
+        rlRun "factorial foo 2>&1 | grep \"Argument doesn't seem to be an integer.\"" 10 \
             "Run factorial with non-numeric arguments"
-        rlAssertGrep "Argument doesn't seem to be an integer." "log"
     rlPhaseEnd
 
     rlPhaseStartCleanup
+        # This section cleans up the environment
+        #
+        # Tato sekce uklidi prostredi
         rlRun "popd"
         rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
     rlPhaseEnd
