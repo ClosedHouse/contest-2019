65fedbecfc3dce7ffd0d846195d26b2257c309ff
Jakub Heger <jheger@redhat.com>
2019-04-12 16:03:48 +0200

    level2: comment test
    add comments to the test to make it easier for the user

--- level2/src/test/runtest.sh
+++ level2/src/test/runtest.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-#   runtest.sh of /CoreOS/tests/Sanity/factorial
 #   Description: Basic sanity tests for factorial package
 #   Author: Anna Khaitovich <akhaitov@redhat.com>
-#
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 set -o pipefail
@@ -14,48 +11,64 @@ set -o pipefail
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 rlJournalStart
+    # This phase performs general testing enviromental setup. You can ignore it.
+    #
+    # Tato faze nastavuje obecne testovaci prostredi. Muzete ji ignorovat.
     rlPhaseStartSetup
-        # This phase performs general testing enviromental setup
-        #
-        # Tato faze nastavuje obecne testovaci prostredi
         rlRun "TmpDir=\$(mktemp -d)" 0 "Creating tmp directory"
         rlRun "pushd $TmpDir"
     rlPhaseEnd
 
-    # TODO rewrite this text, put into individual phases
-    # This section tests factorial package.
-    # There are 7 assertions - commands, that execute specific actions and compare
-    # the result of it with the expected one.
+    # This phase tests basic sanity of package.
+    # There is one failing test case in this phase: factorial is called with non-integer
+    # input which is obviously not valid. This issue is already solved in provided
+    # package with version 1.1.1. Simply install it and this phase should pass.
     #
-    # Tato sekce testuje balicek factorial.
-    # Tady se pouziva 7 assertions - prikazu, ktere vykonavaji nejaky cin a
-    # porovnavaji jeho vysledek s vysledkem, ktery ocekavame.
-    # hint: one of next 3 assertions has wrong expected result
-    # napoveda: jedna s nasledujicich 3 assertions ma spatny ocekavany vysledek
-
+    # Tato faze testuje zakladni funkcnost balicku.
+    # Ve fazi je jeden failujici test case: factorial se vola s necelociselnym
+    # argumentem coz neni validni. Tento problem je vyresen v poskytnutem balicku
+    # s verzi 1.1.1. Staci ho nainstalovat a tato faze by mela projit.
     rlPhaseStartTest "Sanity"
-        rlRun "factorial 2>&1 | grep \"No argument given!\"" 1 "Run factorial with no parameters"
         rlRun "factorial --help | grep \"Usage: factorial INTEGER\"" 0 "Run factorial --help"
+        rlRun "factorial 2>&1 | grep \"No argument given!\"" 1 "Run factorial with no parameters"
         rlRun "factorial foo 2>&1 | grep \"Argument does not seem to be an integer.\"" 10 \
             "Run factorial with non-numeric arguments"
     rlPhaseEnd
 
+    # This phase tests common, expected input.
+    # However QE engineer who wrote this test made a typo and mistakenly expect
+    # wrong value to be returned from factorial. Find the typo and fix it.
+    #
+    # Tato faze testuje bezny, ocekavany vstup.
+    # Nicmene QE inzenyrka ktera napsala tento test udelala preklep a ted ocekava
+    # ze factorial vrati spatnou hodnotu. Najdete ten preklep a opravte ho.
     rlPhaseStartTest "Common values"
         rlAssertEquals "Calculate 3!" $(factorial 3) 6
         rlAssertEquals "Calculate 5!" $(factorial 5) 121
         rlAssertEquals "Calculate 10!" $(factorial 10) 3628800
     rlPhaseEnd
 
+    # This phase tests edge cases.
+    # Developer who wrote the factorial was a bit lazy and didn't cover edge cases
+    # in his implementation. Program doesn't behave correctly for input 0 and for
+    # negative integers. It is up to you to fix the program, build new version of
+    # the package and install it. If you do it correctly, this phase will pass.
+    #
+    # Tato faze testuje edge casy (okrajove hodnoty).
+    # Developer, ktery napsal factorial byl trochu liny a nepokryl edge casy ve
+    # sve implementaci. Program se nechova spravne pro vstup 0 a negativni cela
+    # cisla. Je na vas abyste opravili chovani programu, vybuildili novou verzi
+    # balicku a nainstalovali ji. Pokud to udelate spravne, faze projde.
     rlPhaseStartTest "Edge cases"
         rlAssertEquals "Calculate 1!" $(factorial 1) 1
         rlAssertEquals "Calculate 0!" $(factorial 0) 1
         rlRun "factorial -1" 1-255 "Factorial is not defined for negative integers"
     rlPhaseEnd
 
+    # This phase cleans up the testing environment. You can ignore it.
+    #
+    # Tato faze uklidi testovaci prostredi. Muzete ji ignorovat.
     rlPhaseStartCleanup
-        # This phase cleans up the testing environment
-        #
-        # Tato faze uklidi testovaci prostredi
         rlRun "popd"
         rlRun "rm -r $TmpDir" 0 "Removing tmp directory"
     rlPhaseEnd
